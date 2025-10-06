f4c0d4bea9a3c440230241064cb8cfa2ffe35357
perolavieiraa <perola_vieira@estudante.sesisenai.org.br>
2025-10-06 12:30:17 -0300

    Conexao ao WiFi

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
+void setup() {
+Serial.begin(115200);
+Serial.println("Conectando ao WiFi"); 
+WiFi.begin(SSID,PASS);
+while(WiFi.status() != WL_CONNECTED){
+  Serial.print(".");
+  delay(200);
+}
+Serial.println ("\nConectado!");
 }
 
 void loop() {
