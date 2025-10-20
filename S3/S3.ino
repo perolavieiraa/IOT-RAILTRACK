6ce988cebb58a67ae9e0b77744d519f594c0e9f8
perolavieiraa <perola_vieira@estudante.sesisenai.org.br>
2025-10-20 12:45:36 -0300

    publicação de mensagem

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,25 +1,43 @@
 #include <WiFi.h>
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+#include <PubSubClient.h>
+
+WiFiClient client;          
+PubSubClient mqtt(client);  
+
+const  SSID = "FIESC_IOT_EDU";
+const  PASS = "8120gv08";
+
+const int PORT = 1883;
+const URL = "test.mosquitto.org";
+const TOPIC = "DSM2";
+const broker_user = "";
+const broker_pass = "";
 
 void setup() {
-Serial.begin(115200);
-Serial.println("Conectando ao WiFi"); 
-WiFi.begin(SSID,PASS);
-while(WiFi.status() != WL_CONNECTED){
-  Serial.print(".");
-  delay(200);
-}
-Serial.println ("\nConectado!");
+  Serial.begin(115200);
+  Serial.println("Conectando ao WiFi");
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado!");
+  Serial.println("Conectando ao broker...");
+  mqtt.setServer(URL, PORT);
+  while (!mqtt.connected()) {
+    String ID = "S1 - ";
+    ID += String(random(0xffff), HEX);
+    mqtt.connect(ID.c_str(), broker_user, broker_pass);
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("\nConectado ao broker com sucesso!");
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED){
-Serial.begin(115200);
-Serial.println("Conectando ao WiFi"); 
-WiFi.begin(SSID,PASS);
-while(WiFi.status() != WL_CONNECTED){
-  Serial.print(".");
-  delay(200);
-  }
+  String mensagem = "Perola";
+  mensagem += "oi";
+  mqtt.publish(TOPIC, mensagem.c_str());
+  mqtt.loop();
+  delay(2000);
 }
