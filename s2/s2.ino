09757175abf15bb6801fd65f0fd03e3d3c4ce010
HelenaBohn <helena_b_martins@estudante.sesisenai.org.br>
2025-10-27 12:23:33 -0300

    troca de mensagens

--- s2/s2.ino
+++ s2/s2.ino
@@ -1,43 +1,48 @@
-#include <WiFi.h>
+ #include <WiFi.h>
 #include <PubSubClient.h>
 
 WiFiClient client;          
-PubSubClient mqtt(client);  
+PubSubClient mqtt(client);
 
-const  SSID = "FIESC_IOT_EDU";
-const  PASS = "8120gv08";
+const string broker_user = "";
+const string broker_pass = "";
 
-const int PORT = 1883;
-const URL = "test.mosquitto.org";
-const TOPIC = "DSM2";
-const broker_user = "";
-const broker_pass = "";
+const string MyTopic = "TopicoHelenaa";
+const string OtherTopic = "TopicoPerola";
 
-void setup() {
+void setup(){
   Serial.begin(115200);
   Serial.println("Conectando ao WiFi");
-  WiFi.begin(SSID, PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(SSID,PASS);
+  while (WiFi.status() !=WL_CONNECTED){
     Serial.print(".");
-    delay(200);
+    delay (200);
   }
-  Serial.println("\nConectado!");
-  Serial.println("Conectando ao broker...");
-  mqtt.setServer(URL, PORT);
-  while (!mqtt.connected()) {
-    String ID = "S1 - ";
-    ID += String(random(0xffff), HEX);
-    mqtt.connect(ID.c_str(), broker_user, broker_pass);
-    delay(200);
-    Serial.print(".");
+  Serial.println("\nConectado!")
+  Serial.println ("Conectando ao broker...");]
+  mqtt.setServer(URL.c_str(), PORT);
+  while(!mqtt.connected()){
+    String ID = "S1-"
+    ID+= String (Random(0xffff),HEX);
+    mqtt.connect(ID.c_str(),broker_user.c_str(),broker_pass.c_str());
   }
-  Serial.println("\nConectado ao broker com sucesso!");
 }
-
+ 
 void loop() {
-  String mensagem = "Helena";
-  mensagem += "oi";
-  mqtt.publish(TOPIC, mensagem.c_str());
-  mqtt.loop();
-  delay(2000);
+String mensagem = "Helena: ";
+if(Serial.available()>0){
+mensagem += Serial.readStringUntil('\n');
+mqtt.publish(OtherTopic.c_str(), mensagem.c_str()); 
+}
+mqtt.loop();
+delay(2000);
+I
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+String mensagem = "";
+for(int i = 0 i < length; i++){
+mensagem += (char) payload[i];
+}
+Serial.print("Recebido: ");
+Serial.println(mensagem);
 }
