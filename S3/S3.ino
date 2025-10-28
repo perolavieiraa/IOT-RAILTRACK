a0534fea7b9bf52277f07a41809f05e4927d85f1
perolavieiraa <perola_vieira@estudante.sesisenai.org.br>
2025-10-28 07:23:23 -0300

    Troca de mensagens

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+/* #include <WiFi.h>
 #include <PubSubClient.h>
 
 WiFiClient client;          
@@ -40,4 +40,79 @@ void loop() {
   mqtt.publish(TOPIC, mensagem.c_str());
   mqtt.loop();
   delay(2000);
+} */
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+const SSID = "FIESC_IOT_EDU";
+const PASS = "8120gv08";
+
+const int PORT = 1883;
+const URL = "test.mosquitto.org";
+const MyTopic = "TopicoPerola";
+const OtherTopic = "TopicoHelenaa";
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String mensagem = "";
+  for (int i = 0; i < length; i++) {
+    mensagem += (char)payload[i];
+  }
+
+  Serial.print("Mensagem recebida: ");
+  Serial.println(mensagem);
+
+  if (mensagem == "Acender") {
+    digitalWrite(led, HIGH);
+    Serial.println("LED ligado!");
+  } else if (mensagem == "Apagar") {
+    digitalWrite(led, LOW);
+    Serial.println("LED desligado!");
+  }
+}
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(115200);
+
+  Serial.println("Conectando ao WiFi...");
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+
+  Serial.println("\nWiFi conectado!");
+  mqtt.setServer(URL, PORT);
+  mqtt.setCallback(callback);
+
+  Serial.println("Conectando ao broker MQTT...");
+  while (!mqtt.connected()) {
+    String ID = "ESP32-";
+    ID += String(random(0xffff), HEX);
+    if (mqtt.connect(ID.c_str())) {
+      Serial.println("\nConectado ao broker!");
+    } else {
+      Serial.print(".");
+      delay(500);
+    }
+  }
+
+  mqtt.subscribe(MyTopic);
+  Serial.println("Inscrito no tópico com sucesso!");
+}
+
+void loop() {
+static unsigned long ultimoTempo = 0;
+  if (millis() - ultimoTempo > 5000) {
+    ultimoTempo = millis();
+    String mensagem = "Temperatura: 25°C";
+    mqtt.publish(OtherTopic, mensagem.c_str());
+    Serial.println("Mensagem enviada: " + mensagem);
+  }
+
+  mqtt.loop();
 }
