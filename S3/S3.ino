30efd692a3b0063beec934c99ba88095cadf7bca
perolavieiraa <perola_vieira@estudante.sesisenai.org.br>
2025-11-17 11:23:48 -0300

    PLACA S3

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,118 +1,124 @@
-/* #include <WiFi.h>
-#include <PubSubClient.h>
-
-WiFiClient client;          
-PubSubClient mqtt(client);  
-
-const  SSID = "FIESC_IOT_EDU";
-const  PASS = "8120gv08";
-
-const int PORT = 1883;
-const URL = "test.mosquitto.org";
-const TOPIC = "DSM2";
-const broker_user = "";
-const broker_pass = "";
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Conectando ao WiFi");
-  WiFi.begin(SSID, PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(200);
-  }
-  Serial.println("\nConectado!");
-  Serial.println("Conectando ao broker...");
-  mqtt.setServer(URL, PORT);
-  while (!mqtt.connected()) {
-    String ID = "S1 - ";
-    ID += String(random(0xffff), HEX);
-    mqtt.connect(ID.c_str(), broker_user, broker_pass);
-    delay(200);
-    Serial.print(".");
-  }
-  Serial.println("\nConectado ao broker com sucesso!");
-}
-
-void loop() {
-  String mensagem = "Perola";
-  mensagem += "oi";
-  mqtt.publish(TOPIC, mensagem.c_str());
-  mqtt.loop();
-  delay(2000);
-} */
+// CÓDIGO S3 - SA RAILTRACK
 
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <Servo.h>
 
-WiFiClient client;
-PubSubClient mqtt(client);
+const char* ssid = "FIESC_IOT_EDU";
+const char* password = "8120gv08";
 
-const SSID = "FIESC_IOT_EDU";
-const PASS = "8120gv08";
+const char* mqtt_server = "f518a08331f047ffaba17f3a43c5066c.s1.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_user = "placa3-perola";
+const char* mqtt_password = "Perola2025";
 
-const int PORT = 1883;
-const URL = "test.mosquitto.org";
-const MyTopic = "TopicoPerola";
-const OtherTopic = "TopicoHelenaa";
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  String mensagem = "";
-  for (int i = 0; i < length; i++) {
-    mensagem += (char)payload[i];
-  }
+const int pinSensorPresenca = 15;
+const int pinLEDIlum = 2;
+const int pinServo1 = 4;
+const int pinServo2 = 5;
 
-  Serial.print("Mensagem recebida: ");
-  Serial.println(mensagem);
+Servo servo1;
+Servo servo2;
 
-  if (mensagem == "Acender") {
-    digitalWrite(led, HIGH);
-    Serial.println("LED ligado!");
-  } else if (mensagem == "Apagar") {
-    digitalWrite(led, LOW);
-    Serial.println("LED desligado!");
+bool ledState = false;
+int posServo1 = 0;
+int posServo2 = 0;
+int sensorPresenca = 0;
+
+const char* topicPresenca = "RAILTRACK/S3/Presenca";
+const char* topicServo1 = "RAILTRACK/S3/Servo1";
+const char* topicServo2 = "RAILTRACK/S3/Servo2";
+const char* topicILum = "RAILTRACK/S3/ILum";
+const char* topicStatusLED = "RAILTRACK/S3/StatusLED";
+
+void setup_wifi() {
+  delay(10);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
   }
 }
 
-void setup() {
-  pinMode(led, OUTPUT);
-  Serial.begin(115200);
-
-  Serial.println("Conectando ao WiFi...");
-  WiFi.begin(SSID, PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(200);
+void callback(char* topic, byte* payload, unsigned int length) {
+  String messageTemp;
+  for (unsigned int i = 0; i < length; i++) {
+    messageTemp += (char)payload[i];
   }
 
-  Serial.println("\nWiFi conectado!");
-  mqtt.setServer(URL, PORT);
-  mqtt.setCallback(callback);
+  if (String(topic) == topicILum) {
+    if (messageTemp == "1") {
+      digitalWrite(pinLEDIlum, HIGH);
+      ledState = true;
+    } else if (messageTemp == "0") {
+      digitalWrite(pinLEDIlum, LOW);
+      ledState = false;
+    }
+    client.publish(topicStatusLED, ledState ? "1" : "0");
+  }
 
-  Serial.println("Conectando ao broker MQTT...");
-  while (!mqtt.connected()) {
-    String ID = "ESP32-";
-    ID += String(random(0xffff), HEX);
-    if (mqtt.connect(ID.c_str())) {
-      Serial.println("\nConectado ao broker!");
-    } else {
-      Serial.print(".");
-      delay(500);
+  if (String(topic) == topicServo1) {
+    int pos = messageTemp.toInt();
+    if (pos >= 0 && pos <= 180) {
+      servo1.write(pos);
+      posServo1 = pos;
     }
   }
 
-  mqtt.subscribe(MyTopic);
-  Serial.println("Inscrito no tópico com sucesso!");
+  if (String(topic) == topicServo2) {
+    int pos = messageTemp.toInt();
+    if (pos >= 0 && pos <= 180) {
+      servo2.write(pos);
+      posServo2 = pos;
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ESP32_S3_Client", mqtt_user, mqtt_password)) {
+      client.subscribe(topicILum);
+      client.subscribe(topicServo1);
+      client.subscribe(topicServo2);
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(pinLEDIlum, OUTPUT);
+  digitalWrite(pinLEDIlum, LOW);
+
+  pinMode(pinSensorPresenca, INPUT);
+
+  servo1.attach(pinServo1);
+  servo2.attach(pinServo2);
+
+  setup_wifi();
+
+  espClient.setInsecure();
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
-static unsigned long ultimoTempo = 0;
-  if (millis() - ultimoTempo > 5000) {
-    ultimoTempo = millis();
-    String mensagem = "Temperatura: 25°C";
-    mqtt.publish(OtherTopic, mensagem.c_str());
-    Serial.println("Mensagem enviada: " + mensagem);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  int presencaAtual = digitalRead(pinSensorPresenca);
+  if (presencaAtual != sensorPresenca) {
+    sensorPresenca = presencaAtual;
+    client.publish(topicPresenca, sensorPresenca ? "1" : "0");
   }
 
-  mqtt.loop();
+  delay(1000);
 }
